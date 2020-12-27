01edfbb76400c63ea2e21d692010c999c901cb01
Akira Hayakawa <ruby.wktk@gmail.com>
2020-12-27 13:17:25 +0900

    add abuse-proto

--- abuse-kmod/src/abuse.c
+++ abuse-kmod/src/abuse.c
@@ -1,23 +1,13 @@
 /*
- *  linux/drivers/block/abuse.c
- *
- *  Written by Zachary Amsden, 7/23/2009
- *
- *  This was heavily stolen from pieces of the loopback, network block device,
- *  and parts of FUSE.  Since then it has grown antlers and had several new
- *  limbs grafted onto it, even some of the intenal organs have been replaced.
- *  Please forgive the comments and the obvious uprooting of kernel interfaces.
- *
- *  I believe the module is named appropriately.
- *
- *  The point of this driver is to allow /user-space/ drivers for kernel block
- *  devices.  Yes, it's a strange concept.  However, it's also incredibly
- *  useful.  I would not recommend trying to swap on these devices, unless you
- *  can prove that case deadlock free.
- *
- * Copyright (c) 2009 by Zachary Amsden.  Redistribution of this file is
- * permitted under the GNU General Public License.
- *
+ * This software is a fork of
+ * 
+ * linux/drivers/block/abuse.c
+ * Written by Zachary Amsden, 7/23/2009
+ * 
+ * Copyright (c) 2009 by Zachary Amsden.
+ * Copyright (c) 2021 by Akira Hayakawa.
+ * 
+ * Redistribution of this file is permitted under the GNU General Public License.
  */
 
 #include <linux/version.h>
@@ -31,7 +21,7 @@
 #include <linux/major.h>
 #include <linux/blkdev.h>
 #include <linux/init.h>
-#include <linux/buffer_head.h>		/* for invalidate_bdev() */
+#include <linux/buffer_head.h>
 #include <linux/cdev.h>
 #include <linux/poll.h>
 #include <linux/miscdevice.h>
@@ -66,7 +56,8 @@
 #endif
 
 static DEFINE_MUTEX(abuse_devices_mutex);
-static DEFINE_MUTEX(abctl_mutex);
+// not used
+// static DEFINE_MUTEX(abctl_mutex);
 static DEFINE_IDR(abuse_index_idr);
 static struct class *abuse_class;
 static int max_part;
@@ -76,27 +67,27 @@ static int dev_shift;
 static struct abuse_device *abuse_alloc(int i);
 static void abuse_del_one(struct abuse_device *ab);
 
-/*
- * Add bio to back of pending list
- */
-
-static void abuse_flush_req(struct abuse_device *ab)
+static void abuse_flush_pending_requests(struct abuse_device *ab)
 {
-	struct ab_req *req = NULL, *tmp;
+	struct ab_req *req, *tmp;
+
+	printk("flush");
 
 	spin_lock_irq(&ab->ab_lock);
+	printk("flush X");
 	list_for_each_entry_safe(req, tmp, &ab->ab_reqlist, list) {
+		printk("flush Y");
 		req->rq->rq_flags |= RQF_FAILED;
+		printk("flush Z");
 		compat_complete_request(req->rq);
+		printk("flush A");
 		list_del(&req->list);
 	}
 	spin_unlock_irq(&ab->ab_lock);
 }
-
 static inline int is_abuse_device(struct file *file)
 {
 	struct inode *i = file->f_mapping->host;
-
 	return i && S_ISBLK(i->i_mode) && MAJOR(i->i_rdev) == ABUSE_MAJOR;
 }
 
@@ -118,7 +109,7 @@ static int abuse_reset(struct abuse_device *ab)
 	if (!ab->ab_disk->queue)
 		return -EINVAL;
 
-	abuse_flush_req(ab);
+	abuse_flush_pending_requests(ab);
 	ab->ab_flags = 0;
 	ab->ab_errors = 0;
 	ab->ab_blocksize = 0;
@@ -126,7 +117,10 @@ static int abuse_reset(struct abuse_device *ab)
 	ab->ab_max_queue = 0;
 	set_capacity(ab->ab_disk, 0);
 	if (ab->ab_device) {
+		// https://lkml.org/lkml/2020/8/21/268
+		// should use bd_set_nr_sectors
 		bd_set_size(ab->ab_device, 0);
+		// Invalidate clean unused buffers and pagecache.
 		invalidate_bdev(ab->ab_device);
 		if (max_part > 0)
 			reread_partition(ab->ab_device);
@@ -137,17 +131,15 @@ static int abuse_reset(struct abuse_device *ab)
 	return 0;
 }
 
-static int
-abuse_set_status_int(struct abuse_device *ab, struct block_device *bdev,
-	const struct abuse_info *info)
+static int abuse_set_status_int(struct abuse_device *ab, struct block_device *bdev, const struct abuse_info *info)
 {
+	int err;
+
 	sector_t size = (sector_t)(info->ab_size >> 9);
 	loff_t blocks;
-	int err;
 
 	if (unlikely((loff_t)size != size))
 		return -EFBIG;
-
 	if (unlikely(info->ab_blocksize == 0))
 		return -EINVAL;
 
@@ -155,6 +147,8 @@ abuse_set_status_int(struct abuse_device *ab, struct block_device *bdev,
 	if (unlikely(info->ab_blocksize * blocks != info->ab_size))
 		return -EINVAL;
 
+	// This unlikely happens when ABUSE_SET_STATUS ioctl is called twice.
+	// But this is unusual.
 	if (unlikely(bdev)) {
 		if (bdev != ab->ab_device)
 			return -EBUSY;
@@ -171,6 +165,7 @@ abuse_set_status_int(struct abuse_device *ab, struct block_device *bdev,
 		    info->ab_max_queue > ab->ab_queue_size)
 		    	return -EINVAL;
 	} else {
+		// Acquire the block_device from ab_disk (:: gen_disk)
 		bdev = bdget_disk(ab->ab_disk, 0);
 		if (IS_ERR(bdev)) {
 			err = PTR_ERR(bdev);
@@ -205,6 +200,15 @@ abuse_set_status_int(struct abuse_device *ab, struct block_device *bdev,
 }
 
 static int
+abuse_set_status(struct abuse_device *ab, struct block_device *bdev, const struct abuse_info __user *arg)
+{
+	struct abuse_info info;
+	if (copy_from_user(&info, arg, sizeof (struct abuse_info)))
+		return -EFAULT;
+	return abuse_set_status_int(ab, bdev, &info);
+}
+
+static int
 abuse_get_status_int(struct abuse_device *ab, struct abuse_info *info)
 {
 	memset(info, 0, sizeof(*info));
@@ -220,19 +224,7 @@ abuse_get_status_int(struct abuse_device *ab, struct abuse_info *info)
 }
 
 static int
-abuse_set_status(struct abuse_device *ab, struct block_device *bdev,
-	const struct abuse_info __user *arg)
-{
-	struct abuse_info info;
-
-	if (copy_from_user(&info, arg, sizeof (struct abuse_info)))
-		return -EFAULT;
-	return abuse_set_status_int(ab, bdev, &info);
-}
-
-static int
-abuse_get_status(struct abuse_device *ab, struct block_device *bdev,
-	struct abuse_info __user *arg)
+abuse_get_status(struct abuse_device *ab, struct block_device *bdev, struct abuse_info __user *arg)
 {
 	struct abuse_info info;
 	int err = 0;
@@ -260,6 +252,8 @@ static int abuse_get_req(struct abuse_device *ab, struct abuse_xfr_hdr __user *a
 	if (copy_from_user(&xfr, arg, sizeof (struct abuse_xfr_hdr)))
 		return -EFAULT;
 
+	printk("ab_transfer_address=%lld", xfr.ab_transfer_address);
+
 	spin_lock_irq(&ab->ab_lock);
 	req = list_first_entry_or_null(&ab->ab_reqlist, struct ab_req, list);
 	if (req) {
@@ -270,30 +264,30 @@ static int abuse_get_req(struct abuse_device *ab, struct abuse_xfr_hdr __user *a
 		list_move_tail(&req->list, &ab->ab_reqlist);
 		spin_unlock_irq(&ab->ab_lock);
 
+		// Use the pointer address as the unique id of the request
 		xfr.ab_id = (__u64)req;
 		xfr.ab_command = rq_data_dir(req->rq);
-		xfr.ab_sector = blk_rq_pos(req->rq);
+		xfr.ab_offset = blk_rq_pos(req->rq) << SECTOR_SHIFT;
 		rq_for_each_segment(bvec, req->rq, iter) {
-			ab->ab_xfer[i].ab_len = bvec.bv_len;
+			// physical address of the page
+			ab->ab_xfer[i].ab_address = (__u64)virt_to_phys(page_address(bvec.bv_page));
 			ab->ab_xfer[i].ab_offset = bvec.bv_offset;
+			ab->ab_xfer[i].ab_len = bvec.bv_len;
 			++i;
 		}
-		xfr.ab_vec_count = req->vec_cnt = i;
+		xfr.ab_vec_count = i;
 	} else {
 		spin_unlock_irq(&ab->ab_lock);
-
-		xfr.ab_transfer_address = 0;
-		xfr.ab_vec_count = 0;
+		return -ENOMSG;
 	}
 
 	if (copy_to_user(arg, &xfr, sizeof(xfr)))
 		return -EFAULT;
-	if (xfr.ab_transfer_address &&
-		copy_to_user((__user void *)xfr.ab_transfer_address, ab->ab_xfer,
-			     xfr.ab_vec_count * sizeof(ab->ab_xfer[0])))
+	BUG_ON(xfr.ab_transfer_address == 0);
+	if (copy_to_user((__user void *)xfr.ab_transfer_address, ab->ab_xfer, xfr.ab_vec_count * sizeof(ab->ab_xfer[0])))
 		return -EFAULT;
 
-	return req ? 0 : -ENOMSG;
+	return 0;
 }
 
 static struct ab_req *abuse_find_req(struct abuse_device *ab, __u64 id)
@@ -308,18 +302,16 @@ static struct ab_req *abuse_find_req(struct abuse_device *ab, __u64 id)
 
 static inline void abuse_add_req(struct abuse_device *ab, struct ab_req *req)
 {
+	printk("add req");
 	spin_lock_irq(&ab->ab_lock);
 	list_add_tail(&req->list, &ab->ab_reqlist);
 	spin_unlock_irq(&ab->ab_lock);
 }
 
-static int abuse_put_req(struct abuse_device *ab, struct abuse_xfr_hdr __user *arg)
+static int abuse_put_req(struct abuse_device *ab, struct abuse_completion __user *arg)
 {
-	struct abuse_xfr_hdr xfr;
+	struct abuse_completion xfr;
 	struct ab_req *req = NULL;
-	struct req_iterator iter;
-	struct bio_vec bvec;
-	int i = 0;
 	unsigned long flags;
 
 	if (!arg)
@@ -327,21 +319,15 @@ static int abuse_put_req(struct abuse_device *ab, struct abuse_xfr_hdr __user *a
 	if (!ab)
 		return -ENODEV;
 
-	if (copy_from_user(&xfr, arg, sizeof (struct abuse_xfr_hdr)))
+	if (copy_from_user(&xfr, arg, sizeof (struct abuse_completion)))
 		return -EFAULT;
 
-	/*
-	 * Handle catastrophes first.  Do this by giving them catnip.
-	 */
 	if (unlikely(xfr.ab_result == ABUSE_RESULT_DEVICE_FAILURE)) {
-		abuse_flush_req(ab);
+		abuse_flush_pending_requests(ab);
 		return 0;
 	}
 
-	/*
-	 * Look up the dang thing to make sure the user is telling us
-	 * they've actually completed some work.  It's very doubtful.
-	 */
+	// Find the request to complete
 	spin_lock_irq(&ab->ab_lock);
 	req = abuse_find_req(ab, xfr.ab_id);
 	if (req) {
@@ -352,66 +338,13 @@ static int abuse_put_req(struct abuse_device *ab, struct abuse_xfr_hdr __user *a
 	}
 	spin_unlock_irq(&ab->ab_lock);
 
-	/*
-	 * This isn't just arbitrary anal-retentiveness.  Userspace will
-	 * obviously crash and burn, and so we check all fields as stringently
-	 * as possible to provide some protection against the case when we
-	 * re-use the same bio and some user-tarded program tries to complete
-	 * an historical event.  Better prophylactics are possible, but crazy.
-	 */
-	if (blk_rq_pos(req->rq) != xfr.ab_sector ||
-	    rq_data_dir(req->rq) != xfr.ab_command) {
-		abuse_add_req(ab, req);
-		return -EINVAL;
-	}
-
-	/*
-	 * Now handle individual failures that don't affect other I/Os.
-	 */
 	if (unlikely(xfr.ab_result == ABUSE_RESULT_MEDIA_FAILURE)) {
 		blk_mq_end_request(req->rq, -EIO);
 		return 0;
 	}
 
-	/*
-	 * We've now stolen the bio off the queue.  This is stupid if we don't
-	 * complete it.  But we don't want to hold the spinlock while doing I/O
-	 * from the user component.  If userspace bugs out and crashes, as is
-	 * to be expected from a userspace program, so be it.  The bio can
-	 * always be cancelled by a sane actor when we put it back.
-	 */
-	if (copy_from_user(ab->ab_xfer, (__user void *)xfr.ab_transfer_address,
-			     req->vec_cnt * sizeof(ab->ab_xfer[0]))) {
-		abuse_add_req(ab, req);
-		return -EFAULT;
-	}
-
-	/*
-	 * You made it this far?  It's time for the third movement.
-	 */
-	rq_for_each_segment(bvec, req->rq, iter) {
-		int ret;
-		void *kaddr = kmap(bvec.bv_page);
-
-		if (rq_data_dir(req->rq) != WRITE)
-			ret = copy_from_user(kaddr + bvec.bv_offset,
-					     (void __user *)ab->ab_xfer[i].ab_address,
-					     bvec.bv_len);
-		else
-			ret = copy_to_user((void __user *)ab->ab_xfer[i].ab_address,
-					   kaddr + bvec.bv_offset, bvec.bv_len);
-
-		kunmap(bvec.bv_page);
-		if (ret != 0) {
-			/* Wise, up sucker! (PWEI RULEZ) */
-			abuse_add_req(ab, req);
-			return -EFAULT;
-		}
-		++i;
-	}
-
-	/* Well, you did it.  Congraulations, you get a pony. */
 	compat_spin_lock_irqsave(req->rq->q->queue_lock, flags);
+	printk("complete");
 	blk_mq_end_request(req->rq, 0);
 	compat_spin_unlock_irqrestore(req->rq->q->queue_lock, flags);
 
@@ -432,11 +365,13 @@ static int abuse_acquire(struct file *ctl, unsigned long arg)
 
 	ab = idr_find(&abuse_index_idr, iminor(dev->f_inode));
 	fput(dev);
+
 	if (!ab)
 		return -ENODEV;
 
 	ctl->private_data = ab;
 
+	printk("acquired");
 	return 0;
 }
 
@@ -458,6 +393,8 @@ static long abctl_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	struct abuse_device *new, *remove;
 	int err;
 
+	// ioctl types before ABUSE_CTL_ADD (including GET_REQ, SET_REQ)
+	// are executed sequentially with mutex held.
 	if (cmd < ABUSE_CTL_ADD) {
 		if (ab == NULL)
 			return -EINVAL;
@@ -466,12 +403,10 @@ static long abctl_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 	switch (cmd) {
 	case ABUSE_GET_STATUS:
-		err = abuse_get_status(ab, ab->ab_device,
-				       (struct abuse_info __user *) arg);
+		err = abuse_get_status(ab, ab->ab_device, (struct abuse_info __user *) arg);
 		break;
 	case ABUSE_SET_STATUS:
-		err = abuse_set_status(ab, ab->ab_device,
-				       (struct abuse_info __user *) arg);
+		err = abuse_set_status(ab, ab->ab_device, (struct abuse_info __user *) arg);
 		break;
 	case ABUSE_RESET:
 		err = abuse_reset(ab);
@@ -480,7 +415,7 @@ static long abctl_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		err = abuse_get_req(ab, (struct abuse_xfr_hdr __user *) arg);
 		break;
 	case ABUSE_PUT_REQ:
-		err = abuse_put_req(ab, (struct abuse_xfr_hdr __user *) arg);
+		err = abuse_put_req(ab, (struct abuse_completion __user *) arg);
 		break;
 	case ABUSE_CTL_ADD:
 		mutex_lock(&abuse_devices_mutex);
@@ -489,7 +424,8 @@ static long abctl_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			add_disk(new->ab_disk);
 			err = new->ab_number;
 		} else {
-			err = -EEXIST; /* FIXME: better error handling */
+			// FIXME: better error handling
+			err = -EEXIST;
 		}
 		mutex_unlock(&abuse_devices_mutex);
 		break;
@@ -500,7 +436,6 @@ static long abctl_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			err = -ENOENT;
 		} else {
 			err = remove->ab_number;
-			idr_remove(&abuse_index_idr, remove->ab_number);
 			abuse_del_one(remove);
 		}
 		mutex_unlock(&abuse_devices_mutex);
@@ -527,18 +462,28 @@ static unsigned int abctl_poll(struct file *filp, poll_table *wait)
 	unsigned int mask;
 	struct abuse_device *ab = filp->private_data;
 
+	printk("poll");
+
 	if (ab == NULL)
 		return -ENODEV;
 
+	printk("poll X");
 	poll_wait(filp, &ab->ab_event, wait);
+	printk("poll Y");
 
-	/*
-	 * The comment in asm-generic/poll.h says of these nonstandard values,
-	 * 'Check them!'.  Thus we use POLLMSG to force the user to check it.
-	 */
-	mask = (list_empty(&ab->ab_reqlist)) ? 0 : POLLMSG;
+	mask = (list_empty(&ab->ab_reqlist)) ? 0 : POLLIN;
+	printk("poll mask=%d", mask);
 
 	return mask;
+}
+
+static int abctl_mmap(struct file *filp,  struct vm_area_struct *vma)
+{
+	printk("called mmap");
+	unsigned long size = vma->vm_end - vma->vm_start;
+	unsigned long pfn = vma->vm_pgoff;
+	printk("pfn=%lu", pfn);
+	return remap_pfn_range(vma, vma->vm_start, pfn, size, vma->vm_page_prot);
 }
 
 static int abctl_release(struct inode *inode, struct file *filp)
@@ -585,6 +530,7 @@ static struct file_operations abctl_fops = {
 	.release =		abctl_release,
 	.unlocked_ioctl =	abctl_ioctl,
 	.poll =		abctl_poll,
+	.mmap = abctl_mmap,
 };
 
 static struct miscdevice abuse_misc = {
@@ -595,9 +541,6 @@ static struct miscdevice abuse_misc = {
 
 MODULE_ALIAS("devname:abctl");
 
-/*
- * And now the modules code and kernel interface.
- */
 static int max_abuse;
 module_param(max_abuse, int, 0);
 MODULE_PARM_DESC(max_abuse, "Maximum number of abuse devices");
@@ -611,8 +554,10 @@ static int abuse_init_request(struct blk_mq_tag_set *set, struct request *rq,
 {
 	struct ab_req *req = blk_mq_rq_to_pdu(rq);
 
-	req->rq = rq;
+	printk("init_request");
 
+	INIT_LIST_HEAD(&req->list);
+	req->rq = rq;
 	return 0;
 }
 
@@ -621,13 +566,15 @@ static blk_status_t abuse_queue_rq(struct blk_mq_hw_ctx *hctx, const struct blk_
 	struct ab_req *req = blk_mq_rq_to_pdu(bd->rq);
 	struct abuse_device *ab = req->rq->q->queuedata;
 
+	printk("queue_rq");
+
 	blk_mq_start_request(bd->rq);
 
 	spin_lock_irq(&ab->ab_lock);
+	printk("queue_rq X");
 	list_add_tail(&req->list, &ab->ab_reqlist);
 	wake_up(&ab->ab_event);
 	spin_unlock_irq(&ab->ab_lock);
-
 
 	return BLK_STS_OK;
 }
@@ -637,7 +584,7 @@ static struct blk_mq_ops abuse_mq_ops = {
 	.init_request	= abuse_init_request,
 };
 
-/* FIXME: error propagation */
+// FIXME: error propagation
 static struct abuse_device *abuse_alloc(int i)
 {
 	struct abuse_device *ab;
@@ -683,7 +630,6 @@ static struct abuse_device *abuse_alloc(int i)
 	disk = ab->ab_disk = alloc_disk(num_minors);
 	if (!disk)
 		goto out_free_queue;
-
 	disk->major		= ABUSE_MAJOR;
 	disk->first_minor	= i << dev_shift;
 	disk->fops		= &ab_fops;
@@ -697,6 +643,7 @@ static struct abuse_device *abuse_alloc(int i)
 	spin_lock_init(&ab->ab_lock);
 	INIT_LIST_HEAD(&ab->ab_reqlist);
 
+	printk("ab device allocate ok [%d]", i);
 	return ab;
 
 out_free_queue:
@@ -711,16 +658,7 @@ out:
 	return NULL;
 }
 
-static void abuse_free(struct abuse_device *ab)
-{
-	blk_cleanup_queue(ab->ab_queue);
-	del_gendisk(ab->ab_disk);
-	blk_mq_free_tag_set(&ab->tag_set);
-	put_disk(ab->ab_disk);
-	kfree(ab);
-}
-
-static struct abuse_device *abuse_init_one(int i)
+static struct abuse_device *abuse_add_one(int i)
 {
 	struct abuse_device *ab;
 
@@ -731,12 +669,8 @@ static struct abuse_device *abuse_init_one(int i)
 	ab = abuse_alloc(i);
 	if (ab)
 		add_disk(ab->ab_disk);
-	return ab;
-}
 
-static void abuse_del_one(struct abuse_device *ab)
-{
-	abuse_free(ab);
+	return ab;
 }
 
 static struct kobject *abuse_probe(dev_t dev, int *part, void *data)
@@ -745,13 +679,27 @@ static struct kobject *abuse_probe(dev_t dev, int *part, void *data)
 	struct kobject *kobj;
 
 	mutex_lock(&abuse_devices_mutex);
-	ab = abuse_init_one(dev & MINORMASK);
+	ab = abuse_add_one(dev & MINORMASK);
 	kobj = ab ? compat_get_disk(ab->ab_disk) : ERR_PTR(-ENOMEM);
-
 	mutex_unlock(&abuse_devices_mutex);
 
 	*part = 0;
 	return kobj;
+}
+
+static void abuse_free(struct abuse_device *ab)
+{
+	blk_cleanup_queue(ab->ab_queue);
+	blk_mq_free_tag_set(&ab->tag_set);
+	idr_remove(&abuse_index_idr, ab->ab_number);
+	kfree(ab);
+}
+
+static void abuse_del_one(struct abuse_device *ab)
+{
+	del_gendisk(ab->ab_disk);
+	put_disk(ab->ab_disk);
+	abuse_free(ab);
 }
 
 static int abuse_exit_cb(int id, void *ptr, void *data)
@@ -827,10 +775,7 @@ static int __init abuse_init(void)
 		add_disk(ab->ab_disk);
 	}
 
-	/* point of no return */
-
-	blk_register_region(MKDEV(ABUSE_MAJOR, 0), range,
-				  THIS_MODULE, abuse_probe, NULL, NULL);
+	blk_register_region(MKDEV(ABUSE_MAJOR, 0), range, THIS_MODULE, abuse_probe, NULL, NULL);
 
 	printk(KERN_INFO "abuse: module loaded\n");
 	return 0;
@@ -854,8 +799,10 @@ static void __exit abuse_exit(void)
 
 	idr_for_each(&abuse_index_idr, abuse_exit_cb, NULL);
 	idr_destroy(&abuse_index_idr);
+
 	device_destroy(abuse_class, MKDEV(ABUSECTL_MAJOR, 0));
 	class_destroy(abuse_class);
+
 	blk_unregister_region(MKDEV(ABUSE_MAJOR, 0), range);
 	unregister_chrdev_region(MKDEV(ABUSECTL_MAJOR, 0), range);
 	unregister_blkdev(ABUSE_MAJOR, "abuse");
