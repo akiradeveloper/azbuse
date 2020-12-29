927e6770f24358a9e8a786517844faf5f1de67eb
Akira Hayakawa <ruby.wktk@gmail.com>
2020-12-29 21:13:00 +0900

    update to tokio 1.0

--- nbd/src/transport/mod.rs
+++ nbd/src/transport/mod.rs
@@ -1,6 +1,6 @@
 use std::net::SocketAddr;
 use tokio::net::{TcpStream, TcpListener};
-use tokio::stream::StreamExt;
+use tokio_stream::StreamExt;
 use tokio::sync::mpsc::{self, UnboundedSender, UnboundedReceiver};
 use tokio::io::{ReadHalf, WriteHalf, AsyncWriteExt, AsyncReadExt};
 use crate::{Request, Response, IORequest, IOResponse, IOResponseInner, IORequestInner};
@@ -155,36 +155,31 @@ impl Server {
     }
     pub async fn serve(self, socket: SocketAddr) {
         let mut listener = TcpListener::bind(socket).await.unwrap();
-        while let Some(conn) = listener.next().await {
-            match conn {
-                Ok(mut stream) => {
-                    match handshake(&mut stream, &self.export).await {
-                        Ok(_) => {
-                            let (read_stream, write_stream) = tokio::io::split(stream);
-                            let (response_tx, response_rx) = mpsc::unbounded_channel::<Response>();
-                            let request_handler = RequestHandler {
-                                read_stream,
-                                request_tx: self.request_tx.clone(),
-                                response_tx,
-                            };
-                            let response_handler = ResponseHandler {
-                                write_stream,
-                                response_rx,
-                            };
-                            let fut = async move {
-                                let mut fut1 = tokio::spawn(request_handler.run()).fuse();
-                                let mut fut2 = tokio::spawn(response_handler.run()).fuse();
-                                select! {
-                                    _ = fut1 => {},
-                                    _ = fut2 => {},
-                                }
-                            };
-                            tokio::spawn(fut);
-                        },
-                        Err(_) => {}
-                    }
+        while let Ok((mut stream, _)) = listener.accept().await {
+            match handshake(&mut stream, &self.export).await {
+                Ok(_) => {
+                    let (read_stream, write_stream) = tokio::io::split(stream);
+                    let (response_tx, response_rx) = mpsc::unbounded_channel::<Response>();
+                    let request_handler = RequestHandler {
+                        read_stream,
+                        request_tx: self.request_tx.clone(),
+                        response_tx,
+                    };
+                    let response_handler = ResponseHandler {
+                        write_stream,
+                        response_rx,
+                    };
+                    let fut = async move {
+                        let mut fut1 = tokio::spawn(request_handler.run()).fuse();
+                        let mut fut2 = tokio::spawn(response_handler.run()).fuse();
+                        select! {
+                            _ = fut1 => {},
+                            _ = fut2 => {},
+                        }
+                    };
+                    tokio::spawn(fut);
                 },
-                Err(_) => {},
+                Err(_) => {}
             }
         }
     }
