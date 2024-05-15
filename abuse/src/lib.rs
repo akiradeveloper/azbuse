46573ba82aa5644b020501c2f131fc4058464197
Akira Hayakawa <ruby.wktk@gmail.com>
2024-05-15 20:54:29 +0900

    Delete dbg!

--- abuse/src/lib.rs
+++ abuse/src/lib.rs
@@ -141,7 +141,6 @@ pub async fn run_on(config: Config, engine: impl StorageEngine) {
     info.blocksize = 4096;
     unsafe { abuse_set_status(fd, &info) }.expect("couldn't set info");
 
-    dbg!("start poll");
     let mut poll = Poll::new().unwrap();
     let mut source = SourceFd(&fd);
     poll.registry()
@@ -157,17 +156,14 @@ pub async fn run_on(config: Config, engine: impl StorageEngine) {
         ..AbuseXfr::default()
     };
     loop {
-        dbg!("polling");
         // When there are some requests in the in-kernel queue, this returns events including Token(0).
         // Then the internal loop consumes all requests in the queue.
         poll.poll(&mut events, None).expect("failed to poll");
         'poll: for ev in &events {
-            dbg!(ev);
             loop {
                 if let Err(e) = unsafe { abuse_get_req(fd, &mut xfr) } {
                     break 'poll;
                 }
-                dbg!(&xfr.cmd_flags, &xfr.len);
 
                 let n = xfr.io_vec_count as usize;
                 let xfr_io_vec = {
